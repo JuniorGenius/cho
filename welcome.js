02e4e4e20778db9954a812562afc393ef532075c
JuniorGenius <drgenius@ca.rr.com>
2019-01-04 06:21:25 -0800

    Intro will now only send on join messages
    (Oops)

--- welcome.js
+++ welcome.js
@@ -1,6 +1,8 @@
 const welcome_messages = require('./welcome_messages.json');
 
 async function welcome(client, member, left=false) {
+	const intro = 'Welcome to ***The Starry Expanse Project\'s*** official Discord server!\nPlease make sure to read the rules!'
+	
 	// Get random message
 	const message = welcome_messages[Number(left)][Math.floor(Math.random() * welcome_messages[Number(left)].length)];
 
@@ -17,7 +19,7 @@ async function welcome(client, member, left=false) {
 	// Send greeting on #welcome channel
 	try {
 		const welcome_channel = member.guild.channels.find(ch => ch.name == 'welcome');
-		await welcome_channel.send(message[1].replace('@name', member));
+		await welcome_channel.send(message[1].replace('@name', member) + left ? '' : (' ' + intro));
 	} catch(err) {
 		console.error('Could not send welcome message. Does #welcome exist and do we have permissions?');
 	}
